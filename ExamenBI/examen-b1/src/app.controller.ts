19a907aa2e9a528e2e938cb8f54db85cad4eeca3
Martín Posso <martin.posso@epn.edu.ec>
2025-06-21 22:27:43 -0500

    cambiando el nombre a la funcion rest

--- ExamenBI/examen-b1/src/app.controller.ts
+++ ExamenBI/examen-b1/src/app.controller.ts
@@ -1,26 +1,34 @@
-import { Controller, Get, HttpCode, Query, NotFoundException} from '@nestjs/common';
+import { Controller, Get, HttpCode, Query, NotFoundException } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+
+
+  /*  Realizar servicio REST con la siguiente configuración:
+  - Obtener uno
+  Método: GET
+  URL: /casa
+  Codigo Respuesta: 200
+  Return : [{ id:1, nombre:"Casa 1"}, { id:2, nombre:"Casa 2"}]
+  */
   @Get('/casa')
   @HttpCode(200)
-  ejercicio(
+  getCasa(
     @Query('idCasa') idCasa,
-
-  ): string{
-    const casas:Object[] = [{id:1, nombre:"Casa 1"}, {id:2, nombre:"Casa 2"}];
-    if(idCasa == 1 || idCasa == 2){
-      return JSON.stringify(casas[idCasa-1]);
-    }else{
-      throw new NotFoundException('No econtrado');
+  ): string {
+    const casas: Object[] = [{ id: 1, nombre: "Casa 1" }, { id: 2, nombre: "Casa 2" }];
+    if (idCasa == 1 || idCasa == 2) {
+      return JSON.stringify(casas[idCasa - 1]);
+    } else {
+      throw new NotFoundException('No encontrado');
     }
   }
 }
