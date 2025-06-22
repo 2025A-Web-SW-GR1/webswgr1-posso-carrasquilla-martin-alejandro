f84dd7c7486d7680cd8bb24b485f030efbc13024
Martín Posso <martin.posso@epn.edu.ec>
2025-06-21 21:58:59 -0500

    Añadiendo el examen

---
+++ ExamenBI/examen-b1/src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, HttpCode, Query, NotFoundException} from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/casa')
+  @HttpCode(200)
+  ejercicio(
+    @Query('idCasa') idCasa,
+
+  ): string{
+    const casas:Object[] = [{id:1, nombre:"Casa 1"}, {id:2, nombre:"Casa 2"}];
+    if(idCasa == 1 || idCasa == 2){
+      return JSON.stringify(casas[idCasa-1]);
+    }else{
+      throw new NotFoundException('No econtrado');
+    }
+  }
+}
