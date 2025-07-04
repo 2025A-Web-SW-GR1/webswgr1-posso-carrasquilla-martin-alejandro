e8b1f0020676064249e5896542c79b30598879c5
Martín Posso <martin.posso@epn.edu.ec>
2025-07-04 18:51:26 -0500

    DTO's y Class-Validator

--- 02-servidor/ejemplo-serviodor/src/casa/casa.controller.ts
+++ 02-servidor/ejemplo-serviodor/src/casa/casa.controller.ts
@@ -1,7 +1,14 @@
 import {
-    Controller, Get
+    Body,
+    Controller, Get,
+    Post,
+    Query
 } from '@nestjs/common';
 import { CasaService } from './casa.service';
+import { FindManyOptions, Like } from 'typeorm';
+import { BuscarDto } from './dto/buscar.dto';
+import { Casa } from './casa.entity';
+import { CrearEditarBaseDto } from './dto/crear-editar.base';
 
 @Controller('api/casa')
 export class CasaController {
@@ -11,8 +18,29 @@ export class CasaController {
 
     }
     @Get()
-    obtener() {
-        return this.casaService.obtenerTodos();
+    obtener(
+        @Query() parametrosConsulta:BuscarDto,
+    ) {
+        const objetoBuqueda: FindManyOptions<Casa> = {};
+        if (parametrosConsulta.nombre) {
+            objetoBuqueda.where= {
+                nombre: Like("%" + parametrosConsulta.nombre + "%"),
+            }
+        }
+        return this.casaService.obtenerTodos(objetoBuqueda);
+    }
+
+    @Post()
+    crearUno(
+        @Body() parametrosCuerpo: CrearEditarBaseDto
+    ){
+
+        return this.casaService.crearUno(
+            parametrosCuerpo.nombre, 
+            parametrosCuerpo.valor, 
+            parametrosCuerpo.imagenUrl
+        );
+
     }
 
 }
---
+++ 02-servidor/ejemplo-serviodor/src/casa/dto/crear-editar.base.ts
@@ -0,0 +1,13 @@
+import { Length, IsInt, IsUrl, Min } from "class-validator";
+
+export class CrearEditarBaseDto {
+    @Length(3, 500)
+    nombre: string;
+
+    @IsInt()
+    @Min(0)
+    valor: number;
+
+    @IsUrl()
+    imagenUrl: string;
+}
