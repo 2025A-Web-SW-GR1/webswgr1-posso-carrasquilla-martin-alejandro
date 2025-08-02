525c7311772f4f2ec627faa5f988d0535726ab79
Martín Posso <martin.posso@epn.edu.ec>
2025-08-02 17:31:22 -0500

    Trabajo en clase Guias

--- 02-servidor/ejemplo-serviodor/src/casa/casa.entity.ts
+++ 02-servidor/ejemplo-serviodor/src/casa/casa.entity.ts
@@ -3,7 +3,7 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Casa {
     @PrimaryGeneratedColumn()
-    id: number;
+    id?: number;
 
     @Column({ length: 500 })
     nombre: string;
@@ -11,6 +11,23 @@ export class Casa {
     @Column()
     valor: number;
 
-    @Column()
-    imagenURL: string;
+    @Column({ nullable: true })
+    imagenURL?: string;
+
+    @Column({ nullable: true })
+    fileContentType?: string;
+
+    @Column({ nullable: true })
+    filename?: string;
+
+    @Column({ nullable: true })
+    fileID?: string;
+
+    // Usado para los datos de sesion
+
+    @Column({ type: 'varchar', length: 100 })
+    username: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    password: string;
 }
